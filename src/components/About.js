6092e43c571b3d103f74c2b810776dfd147a4e5f
Lundet <hannes.j.lundin@gmail.com>
2024-06-12 15:02:49 +0200

    footer changes

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,12 @@ function About() {
           Hey there! I'm Hannes Lundin, a 25-year-old web developer based in Helsingborg. I'm passionate about coding and creating cool stuff. My journey into programming started with gaming and game development during high school, and eventually, I ventured into web development as well.
         </p>
         <p class="about-text">
+          I'm primarily seeking freelance opportunities where I can work independently and remotely. However, I'm open to exploring various job opportunities that align with my skills and interests.
+        </p>
+        <p class="about-text">
+          While I'm constantly striving to improve my skills, I find styling to be particularly challenging. Currently, I'm focused on mastering React, and this very site you're on is built using React.
+        </p>
+        <p class="about-text">
           When I'm not coding, you can catch me playing games like League of Legends or experimenting with music production using FL Studio. I'm always up for a challenge and enjoy pushing my limits in both gaming and music.
         </p>
         <p class="about-text">
@@ -17,12 +23,6 @@ function About() {
         </p>
         <p class="about-text">
           Music has always been a passion of mine, and I've recently started creating remixes for my own enjoyment. It's an exciting complement to my web development journey, adding another dimension to my creative pursuits.
-        </p>
-        <p class="about-text">
-          While I'm constantly striving to improve my skills, I find styling to be particularly challenging. Currently, I'm focused on mastering React, and this very site you're on is built using React.
-        </p>
-        <p class="about-text">
-          I'm primarily seeking freelance opportunities where I can work independently and remotely. However, I'm open to exploring various job opportunities that align with my skills and interests.
         </p>
       </section>
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import '../styles/footer.css';
+
 function Footer() {
   return (
     <footer>
-      <p>&copy; Hannes Lundin</p>
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
+        <a href="mailto:hannes.j.lundin@gmail.com">hannes.j.lundin@gmail.com</a>
+        <p>&copy; Hannes Lundin</p>
+      </div>
     </footer>
   );
 }
