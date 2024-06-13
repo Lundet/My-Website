9be6452887d75b3684f970e60e56f9b6f3648d0f
Lundet <hannes.j.lundin@gmail.com>
2024-06-13 14:38:40 +0200

    media queary for footer and added styling to hobbies

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,16 +1,16 @@
-// Hobbies.js
 import React from 'react';
 import '../styles/hobbies.css';
+
 function Hobbies() {
   return (
-    <div className="main">
+    <div id="hobbies">
       <section>
         <h2>Hobbies</h2>
         <p>
           When I'm not working on web development, I enjoy a variety of hobbies that help me stay creative and balanced.
         </p>
 
-        <h1>Esports and Streaming</h1>
+        <h2>Esports and Streaming</h2>
         <p>
           I love esports and have spent a lot of time playing competitively, especially in League of Legends where I've reached Master Tier. This shows my competitive spirit, strategic thinking, and dedication.
         </p>
@@ -18,13 +18,10 @@ function Hobbies() {
           I also enjoy sharing my gaming experiences with others. You can find my live streams on <a href="https://www.twitch.tv/lundet" target="_blank" rel="noopener noreferrer">Twitch</a> and my recorded content on <a href="https://www.youtube.com/channel/UCRe_1SKE84GFzgbpG_eEPPA" target="_blank" rel="noopener noreferrer">YouTube</a>.
         </p>
 
-
-        <h1>Music Production</h1>
+        <h2>Music Production</h2>
         <p>
           I enjoy making music with FL Studio and playing guitar in my free time. It's a great way to unwind and express myself. Music allows me to take a break from coding and have some creative fun.
         </p>
-
-        {/* Add any additional hobbies or interests here */}
       </section>
     </div>
   );
