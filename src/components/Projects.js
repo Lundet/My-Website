33ee05f0170ff45c660d3fab8d097cdb80867f98
Lundet <hannes.j.lundin@gmail.com>
2024-06-19 14:38:09 +0200

    url for solbarn

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,12 +15,14 @@ function Projects() {
         Here are some of the projects I've worked on. You can also find the source code on GitHub: <a href={"https://github.com/Lundet?tab=repositories"} target="_blank" rel="noopener noreferrer">View on GitHub</a>
       </p>
       <ul>
-      <li>
-      <div>
+        <li>
+          <div>
             <h3>Preschool Site</h3>
             <p>An end project for school, this preschool site is designed to be user-friendly and informative for both parents and teachers.</p>
             <p>Developed with HTML, CSS, and JavaScript, the site includes features like class schedules, teacher bios, and a gallery of student activities.</p>
             <p>I designed the entire project from scratch and implemented a custom theme in WordPress.</p>
+            <p><a href="https://solbarn.se/" target="_blank" rel="noopener noreferrer">https://solbarn.se/</a></p>
+
           </div>
           <img src={preschoolSiteImage} alt="Preschool Site" />
         </li>
@@ -29,15 +31,15 @@ function Projects() {
             <h3>Canvas Game</h3>
             <p>A browser-based game created using HTML5 Canvas and JavaScript.</p>
             <p>During this project I learned:</p>
-              <ul>
-                <li>Implementing websockets for real-time communication</li>
-                <li>Using HTML5 Canvas for dynamic graphical elements</li>
-                <li>Creating a well-documented codebase with regular git commits</li>
-                <li>Developing a user-friendly interface</li>
-                <li>Designed my own character and map</li>
-                <li>Techniques for collision detection and animations</li>
-                <li>Creating a lively chat functionality</li>
-              </ul>
+            <ul>
+              <li>Implementing websockets for real-time communication</li>
+              <li>Using HTML5 Canvas for dynamic graphical elements</li>
+              <li>Creating a well-documented codebase with regular git commits</li>
+              <li>Developing a user-friendly interface</li>
+              <li>Designed my own character and map</li>
+              <li>Techniques for collision detection and animations</li>
+              <li>Creating a lively chat functionality</li>
+            </ul>
           </div>
           <img src={canvasGameImage} alt="Canvas Game" />
         </li>
