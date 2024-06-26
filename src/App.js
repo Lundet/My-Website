db030b39b6591a7ad481c7c0206a19cf63fbe359
Lundet <hannes.j.lundin@gmail.com>
2024-06-26 11:34:39 +0200

    googole analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -10,12 +11,17 @@ import Hobbies from './components/Hobbies';
 import Resume from './components/Resume';
 import './App.css';
 
+const TRACKING_ID = "G-TSN98DZJ0L"; // Ersätt med ditt Google Analytics mätning-ID
+
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <main>
+          <Analytics />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -28,6 +34,17 @@ function App() {
       </div>
     </Router>
   );
+}
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
+
+  return null;
 }
 
 export default App;
