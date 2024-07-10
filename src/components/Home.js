babcbcb32c3859d17ff64c6df2b9cfb21250b57b
Lundet <hannes.j.lundin@gmail.com>
2024-07-10 14:29:55 +0200

    added socials

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,6 @@ function Home() {
   return (
     <div className="home-container">
       <Helmet>
-        <html lang="sv" />
         <title>Anlita en Webbutvecklare i Helsingborg, Skåne | Hannes Lundin</title>
         <meta name="description" content="Hannes Lundin - Webbutvecklare specialiserad på HTML, CSS, JavaScript, PHP, MySQL och Node.js. Utforska mitt portfolio, projekt och hobbyer. Anlita en erfaren webbutvecklare i Helsingborg, Skåne eller andra delar av Sverige." />
         <meta name="keywords" content="anlita webbutvecklare, hyr webbutvecklare, webbutvecklare, webbutvecklare Helsingborg, webbutvecklare Skåne, webdesigner Helsingborg, webdesigner Skåne, frilans webbutvecklare, webbutveckling, WordPress utvecklare, hire a web developer, Helsingborg, Skåne, Sverige, webbutvecklare Stockholm, webbutvecklare Göteborg, web designer Malmö, HTML, CSS, JavaScript, PHP, MySQL, Node.js, webbdesign, webbdesigner, hemsida utvecklare, frilansare, hemsida, webbsida, webbapplikation, fullstack utvecklare, frontend utvecklare, backend utvecklare" />
@@ -22,25 +21,28 @@ function Home() {
         <meta property="og:site_name" content="Hannes Lundins Portfolio" />
         <meta property="og:locale" content="sv_SE" />
       </Helmet>
+
+      {/* Your JSX content for the Home component */}
       <div className="main-content">
         <div className="text-section">
           <h2>Hannes Lundin</h2>
           <h2>Welcome to My Website</h2>
           <p>
             Hello! My name is Hannes Lundin, and I'm a passionate web developer with a background in game development and technology. I thrive on challenges and have a strong foundation in HTML, CSS, JavaScript, PHP, MySQL, and Node.js. My journey into coding began in high school, and I've been on a quest for knowledge ever since.
-          </p>    
+          </p>
           <Link to="/about" className="about-button">
             <p>Learn more about me</p>
           </Link>
         </div>
         <div className="picture-section">
-        <img src={profilePicture} alt="Profile of Hannes Lundin" className="profile-picture" loading="lazy" />
-
+          <img src={profilePicture} alt="Profile of Hannes Lundin" className="profile-picture" loading="lazy" />
         </div>
         <div className="skills-section">
           <Skills />
         </div>
       </div>
+
+      {/* Quick Links Navigation */}
       <h2>Quick Links</h2>
       <div className="navigation-cards">
         <Link to="/projects" className="card">
